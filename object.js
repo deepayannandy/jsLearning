a449213ca4b238f899219d17e09508bbecb813b5
Deepayan Nandy <deepayan.622@gmail.com>
2021-11-29 03:08:14 +0530

    update

--- object.js
+++ object.js
@@ -25,3 +25,39 @@ for(let key in person){
 }
 
 console.log(Object.keys(person))
+
+//Computed properties
+const key1="First name"
+const key2="Last name"
+
+const val1="Deepayan"
+const val2="Nandy"
+
+const obj={
+    [key1]: val1,
+    [key2]: val2
+}
+console.log(obj);
+
+// spread operater
+const obj1={
+    key1: "Value1",
+    key2: "Value2",
+    key3: "Value3",
+}
+console.log(obj1);
+
+const newobj={...obj,...obj1}
+console.log(newobj);
+
+// object in array
+const users=[
+    {uname: "Deepayan Nandy", agr:23,userid:1},
+    {uname: "Lipi Thakur", agr:21,userid:2},
+    {uname: "Mrinal Singh", agr:23,userid:3},
+    {uname: "Chandan Gope", agr:23,userid:4},
+]
+console.log(users);
+for(let user of users){
+    console.log(user);
+}
