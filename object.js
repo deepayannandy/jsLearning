a54beb845823b0734ac220f5b8453d815bfe54cc
Deepayan Nandy <deepayan.622@gmail.com>
2021-11-29 02:50:45 +0530

    update

--- object.js
+++ object.js
@@ -8,3 +8,20 @@ console.log(person);
 // get data
 console.log(person.fullname)
 console.log(person.age)
+
+console.log(person["fullname"])
+console.log(person["age"])
+
+// add data
+person.gender="male";
+console.log(person);
+person["Status"]="Single";
+console.log(person);
+
+//loop
+for(let key in person){
+    console.log(key,':',person[key]);
+    console.log(`${key} == ${person[key]}`);
+}
+
+console.log(Object.keys(person))
